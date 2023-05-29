564369128e2e2dfdd9a60be340db46a2cf38195b
Lucas Natan <lucasnatan.dev@gmail.com>
2023-05-28 22:41:47 -0400

    updated project - final lesson

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './header.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-  constructor() {
+  constructor(private headerService: HeaderService) {
 
   }
 
@@ -15,4 +16,15 @@ export class HeaderComponent {
 
   }
 
+  get title(): string {
+    return this.headerService.headerData.title
+  }
+
+  get icon(): string {
+    return this.headerService.headerData.icon
+  }
+
+  get routeUrl(): string {
+    return this.headerService.headerData.routeUrl
+  }
 }
